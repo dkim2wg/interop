125bbe830517fd4e542ddf82cabf392fe902ec27
Bron Gondwana <brong@fastmailteam.com>
2025-11-01 21:40:56 -0400

    bdns2.pl - simple script to split out the DNS in bind zone format

---
+++ util/bdn2.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl -w
+
+use 5.020;
+use JSON;
+
+my %data;
+for my $file (glob "keys/*.pem") {
+   next unless $file =~ m{^keys/([^.]+)\._domainkey\.(.*)\.pem$};
+   my ($sel, $dom) = ($1, $2);
+   my $size = -s $file;
+   # smaller than 500 bytes are ed25519
+   my $r;
+   if ($size < 500) {
+      # https://www.mailhardener.com/kb/how-to-use-dkim-with-ed25519
+      my $p = `openssl asn1parse -in $file -offset 12 -noout -out /dev/stdout | openssl base64`;
+      chomp $p;
+      $r = "v=DKIM1; k=ed25519; p=$p";
+   }
+   else {
+      # https://www.mailhardener.com/kb/how-to-create-a-dkim-record-with-openssl
+      my $p = `openssl ec -in $file -pubout -outform der 2>/dev/null | openssl base64 -A`;
+      chomp $p;
+      $r = "v=DKIM1; k=rsa; p=$p";
+   }
+   push @{$data{$dom}{"$sel._domainkey"}}, ['txt', $r];
+   my ($start) = split /\./, $dom;
+   say qq{$sel._domainkey.$dom. 3600 IN TXT "$r"};
+}
+
+#say JSON->new->canonical->pretty->encode(\%data);
